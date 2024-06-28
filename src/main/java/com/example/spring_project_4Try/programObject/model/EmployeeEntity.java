19e545972b51fc1d8738043c2538787cfde271bb
petrenko_da <diabloo-x@mail.ru>
2024-06-28 16:28:55 +0600

    Add createdAt and createdUp

--- src/main/java/com/example/spring_project_4Try/programObject/model/EmployeeEntity.java
+++ src/main/java/com/example/spring_project_4Try/programObject/model/EmployeeEntity.java
@@ -2,27 +2,26 @@ package com.example.spring_project_4Try.programObject.model;
 
 import com.example.spring_project_4Try.enumeration.StatusEmployee;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jdk.jfr.Timestamp;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.UUID;
 
 @Data
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class EmployeeEntity {
 
     @Id
@@ -33,8 +32,8 @@ public class EmployeeEntity {
     private Integer telephone;
     @Enumerated
     private StatusEmployee status;
-    @CreationTimestamp
+    @CreatedDate
     private LocalDateTime createdAt;
-    @UpdateTimestamp
+    @LastModifiedDate
     private LocalDateTime createdUp;
 }
